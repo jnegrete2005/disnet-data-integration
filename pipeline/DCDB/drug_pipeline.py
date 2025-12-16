12c47556083e7ce865ea8f01bc80be0e5edd96da
Joaquin Negrete <joaquin.negrete03@gmail.com>
2025-12-16 22:31:30 +0100

    Dive drug_pipeline onto it's own file

--- pipeline/DCDB/drug_pipeline.py
+++ pipeline/DCDB/drug_pipeline.py
@@ -1,3 +1,5 @@
+from chembl_webresource_client.new_client import new_client
+
 from pipeline.base_pipeline import IntegrationPipeline
 
 from apis.dcdb import DrugCombDBAPI
@@ -8,50 +10,6 @@ from domain.models import Drug
 from repo.drug_repo import DrugRepo, ForeignMap
 
 from infraestructure.database import DisnetManager
-
-from chembl_webresource_client.new_client import new_client
-
-
-class DrugCombDBPipeline(IntegrationPipeline):
-    """
-    Integrate drug combination data from DrugCombDB into DISNET.
-
-    The pipeline will follow these steps:
-    1. Extract drug combination data from DrugCombDB using the DrugCombDBAPI.
-
-    CELL_LINE AND DISEASE:
-    2. Get the cell line's ID from Cellosaurus
-    3. Transform the NCIt disease IDs to UMLS CUIs using the UMLS API.
-    4. Load the transformed data into the DISNET database.
-
-    DRUGS:
-    2. Get the drug's ID from PubChem through the DrugCombDB API.
-    3. Transform PubChem IDs to CHEMBL IDs using the ChEMBL API.
-    4. Load the transformed data into the DISNET database.
-
-    SCORES:
-    2. Get the scores from the drug combination.
-
-    FINALLY:
-    5. Load all the data into the DISNET database.
-    """
-
-    def run(self, start: int = 1, end: int = 2, step: int = 1):
-        # Step 1: Extract drug combination data from DrugCombDB
-        dcdb_api = DrugCombDBAPI()
-
-        for i in range(start, end, step):
-            drugcomb = dcdb_api.get_drug_combination(i)
-            if not drugcomb:
-                continue
-
-            drugs = [drugcomb.drug1, drugcomb.drug2]
-
-            for drug in drugs:
-                if "(approved)" in drug:
-                    drug = drug.replace("(approved)", "").strip()
-
-                DrugPipeline.run(drug.drug_name)
 
 
 class DrugPipeline(IntegrationPipeline):
