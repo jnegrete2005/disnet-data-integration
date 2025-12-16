a2f45efb6998eceea534bbbce60db8f71d8c1bf1
Joaquin Negrete <joaquin.negrete03@gmail.com>
2025-12-16 22:28:48 +0100

    Use GenericRepo in sql_op decorator

--- repo/base.py
+++ repo/base.py
@@ -1,21 +1,23 @@
-from mysql.connector import Error
 from mysql.connector.cursor import MySQLCursor
 from functools import wraps
 
 from typing import TypeVar, Callable
 from typing_extensions import ParamSpec, Concatenate
 
+from repo.generic_repo import GenericRepo
+
 P = ParamSpec("P")
 R = TypeVar("R")
 
 
 def sql_op(*, returns_bool: bool = True) -> Callable[
-    [Callable[Concatenate["RepoClass", MySQLCursor, P], R]],
-    Callable[Concatenate["RepoClass", P], R]
+    [Callable[Concatenate[GenericRepo, MySQLCursor, P], R]],
+    Callable[Concatenate[GenericRepo, P], R]
 ]:
     return_value = False if returns_bool else None
 
-    def decorator(method: Callable[Concatenate["RepoClass", MySQLCursor, P], R]) -> Callable[Concatenate["RepoClass", P], R]:
+    def decorator(method: Callable[Concatenate[GenericRepo, MySQLCursor, P], R]) -> Callable[Concatenate[GenericRepo, P], R]:
+        @wraps(method)
         def wrapper(self, *args: P.args, **kwargs: P.kwargs) -> R:
             conn = self.db.conn
             cursor = self.db.get_cursor()
