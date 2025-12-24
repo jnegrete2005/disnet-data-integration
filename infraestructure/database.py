07d765a719a4ade7cb39c2d6587fcf035dcf4906
Joaquin Negrete <joaquin.negrete@gmail.com>
2025-12-24 12:39:36 +0100

    Created and tested cell line repo
    From this commit, the development of the APIs and pipelines will be
    halted, until a commit includes these files. I will only be developing
    (and fixing) the repo, domain and infraestructure modules.
    
    For this reason (while the content on APIs is ok) the content on the
    pipeline module is not to be taken as valid, but as extremely buggy
    code.

--- infraestructure/database.py
+++ infraestructure/database.py
@@ -1,13 +1,20 @@
 import mysql.connector
 from mysql.connector import Error
+from mysql.connector.connection import MySQLConnection
+
 import os
 from dotenv import load_dotenv
 
+from pathlib import Path
+
 
 class DisnetManager:
-    def __init__(self):
-        load_dotenv("../.env")
+    def __init__(self, test=False):
+        project_root = Path(__file__).resolve().parents[1]
+        if not load_dotenv(project_root / ".env"):
+            raise Error("ENV VARS NOT LOADED")
         self._conn = None
+        self.__test = test
 
     def connect(self):
         if self._conn is not None:
@@ -17,10 +24,11 @@ class DisnetManager:
                 self._conn = None
 
         try:
+            database = "drugslayer_test" if self.__test else "drugslayer"
             self._conn = mysql.connector.connect(
                 host='127.0.0.1',
                 port=3306,
-                database='drugslayer',
+                database=database,
                 user=os.getenv('DB_USER'),
                 password=os.getenv('DB_PASSWORD')
             )
@@ -34,7 +42,7 @@ class DisnetManager:
             print("Conexión cerrada.")
 
     @property
-    def conn(self):
+    def conn(self) -> MySQLConnection | None:
         if self._conn is None or not self._conn.is_connected():
             return None
         return self._conn
