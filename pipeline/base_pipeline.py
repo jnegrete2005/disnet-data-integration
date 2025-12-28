46d0f06e2ae6462356a1ca98fb62717e67d3dbdb
Joaquin Negrete <joaquin.negrete@gmail.com>
2025-12-28 22:53:58 +0100

    Add db as a param to base pipeline

--- pipeline/base_pipeline.py
+++ pipeline/base_pipeline.py
@@ -1,7 +1,13 @@
 from abc import ABC, abstractmethod
 
+from infraestructure.database import DisnetManager
+
 
 class IntegrationPipeline(ABC):
+    @abstractmethod
+    def __init__(self, db: DisnetManager):
+        pass
+
     @abstractmethod
     def run(self):
         pass
