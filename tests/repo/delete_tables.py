7b6601fe4b7539ad27431deb232a9e8de17c0d5f
Joaquin Negrete <joaquin.negrete@gmail.com>
2025-12-28 16:14:58 +0100

    Created this function to remove everything from the test db

---
+++ tests/repo/delete_tables.py
@@ -0,0 +1,41 @@
+from infraestructure.database import DisnetManager
+
+
+def delete_tables():
+    db = DisnetManager(test=True)
+    db.connect()
+
+    cursor = db.get_cursor()
+
+    try:
+        tables = [
+            "foreign_to_chembl",
+            "drug_raw",
+            "experiment_score",
+            "experiment",
+            "experiment_source",
+            "experiment_classification",
+            "drug_comb_drug",
+            "drug_combination",
+            "score",
+            "cell_line",
+        ]
+        for table in tables:
+            cursor.execute(
+                f"DROP TABLE IF EXISTS {table}"
+            )
+        db.conn.commit()
+
+        cursor.execute("DELETE FROM drug")
+        cursor.execute("DELETE FROM source")
+
+        db.conn.commit()
+    finally:
+        cursor.close()
+        db.disconnect()
+
+    return
+
+
+if __name__ == "__main__":
+    delete_tables()
