ac35ee0ead787d74d0bfdf6558ae3dead2105af5
Joaquin Negrete <joaquin.negrete03@gmail.com>
2025-12-28 19:10:35 +0100

    Only return cellosaurus accession for the cell_line_info

--- apis/dcdb.py
+++ apis/dcdb.py
@@ -10,7 +10,7 @@ class DrugCombDBAPI(APIInterface):
     def __init__(self):
         super().__init__(base_url="http://drugcombdb.denglab.org:8888/")
 
-    def get_drug_combination(self, index: int) -> DrugCombData:
+    def get_drug_combination_info(self, index: int) -> DrugCombData:
         endpoint = f"integration/list/{index}"
         url = f"{self.base_url}{endpoint}"
         response = requests.get(url)
@@ -55,7 +55,4 @@ class DrugCombDBAPI(APIInterface):
         if api_response.code != 200 or api_response.data is None:
             raise ValueError(f"API returned error code {api_response.code}: {api_response.msg}")
 
-        return {
-            "cosmic_id": api_response.data.get("cosmicId"),
-            "cellosaurus_id": api_response.data.get("cellosaurus_assession")
-        }
+        return api_response.data.get("cellosaurus_accession")
